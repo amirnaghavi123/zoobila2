58eb3a34ce440430a8a58289d5b412602e7c20a2
amirnaghavi123 <ana49@sfu.ca>
2015-07-06 11:51:34 -0700

    just a change

---
+++ app/src/main/java/com/amirnaghavi/quiz_activity/Constants.java
@@ -0,0 +1,20 @@
+package com.amirnaghavi.quiz_activity;
+
+/**
+ * Created by amir on 2015-07-06.
+ */
+public class Constants {
+
+    public static final String DATABASE_NAME = "game";
+    public static final String TABLE_NAME = "questions";
+
+    public static final String UID = "_id";
+    public static final String QUESTION = "QUESTION";
+    public static final String OPTION_A = "OPTION_A";
+    public static final String OPTION_B = "OPTION_B";
+    public static final String OPTION_C = "OPTION_C";
+    public static final String OPTION_D = "OPTION_D";
+    public static final String ANSWER = "ANSWER";
+
+    public static final int DATABASE_VERSION = 1;
+}
---
+++ app/src/main/java/com/amirnaghavi/quiz_activity/myHelper.java
@@ -0,0 +1,7 @@
+package com.amirnaghavi.quiz_activity;
+
+/**
+ * Created by amir on 2015-07-06.
+ */
+public class myHelper {
+}
