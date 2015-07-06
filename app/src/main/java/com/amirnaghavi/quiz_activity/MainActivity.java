9dd9110bc14ea042c6c2ea8f9fc17b60607d7064
amirnaghavi123 <ana49@sfu.ca>
2015-07-05 22:38:57 -0700

    just a change

--- app/src/main/java/com/amirnaghavi/quiz_activity/MainActivity.java
+++ app/src/main/java/com/amirnaghavi/quiz_activity/MainActivity.java
@@ -1,17 +1,30 @@
 package com.amirnaghavi.quiz_activity;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    Button quitbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        quitbtn = (Button)findViewById(R.id.quit_btn);
+        quitbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Game_Menu.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
