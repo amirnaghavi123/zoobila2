e81c25768d1fd58e020530c1bbdb67468c868132
amirnaghavi123 <ana49@sfu.ca>
2015-07-05 20:54:03 -0700

    just a change

--- app/src/main/java/com/amirnaghavi/quiz_activity/Instructions.java
+++ app/src/main/java/com/amirnaghavi/quiz_activity/Instructions.java
@@ -3,18 +3,29 @@ package com.amirnaghavi.quiz_activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class Instructions extends ActionBarActivity {
+
+    Button playbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
 
-        Intent intent = new Intent(Instructions.this,MainActivity.class);
-        startActivity(intent);
+
+        playbtn = (Button)findViewById(R.id.play_btn);
+        playbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Instructions.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 
