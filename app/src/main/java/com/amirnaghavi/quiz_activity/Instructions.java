3c23605b0ea61a3d0d6e97ac31f3e8cd2cced741
amirnaghavi123 <ana49@sfu.ca>
2015-07-05 20:51:39 -0700

    just a change

--- app/src/main/java/com/amirnaghavi/quiz_activity/Instructions.java
+++ app/src/main/java/com/amirnaghavi/quiz_activity/Instructions.java
@@ -1,5 +1,6 @@
 package com.amirnaghavi.quiz_activity;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
@@ -11,6 +12,9 @@ public class Instructions extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
+
+        Intent intent = new Intent(Instructions.this,MainActivity.class);
+        startActivity(intent);
     }
 
 
